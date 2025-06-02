879f60d45b2b15869ebb88b72f1b5b9b7af01055
Le Toan <letoan285@gmail.com>
2025-06-02 17:41:25 +0700

    get message

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 // import { useWebSocket } from './hooks/useWebSocket';
 import './App.css';
 import { useWebSocket } from './hooks/hooks_useWebsocket';
-
+import axios from 'axios';
 type Message = {
   id: string;
   content: string;
@@ -31,16 +31,37 @@ export type WebSocketMessageResult = Omit<WebSocketMessage, 'message_type'> & {
 
 // const SOCKET_URL = 'ws://146.190.86.208:8088/ws/1'; http://146.190.86.208:8088/api/auth/login
 // const SOCKET_URL = 'ws://146.190.86.208:8088/ws/1';
-const ROOM_ID = 3;
-const USER_ID = 2;
-const SOCKET_URL = `ws://localhost:8000/ws/${USER_ID}`;
-const URL = 'http://localhost:8000/api';
-const TOKEN =
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NDkyODIxNjgsImlhdCI6MTc0ODY3NzM2OCwiZW1haWwiOiJ0ZXN0LXhhdGhjcmFsckBzcnYxLm1haWwtdGVzdGVyLmNvbSIsImlkIjoyfQ.DGdyaVUffHR5W6uk1465Um8E6dhlgRT1DkXrU9XINaQ';
+const ROOM_ID = 1;
+const USER_ID = 1;
+const SOCKET_URL = `ws://146.190.86.208:8088/ws/${USER_ID}`;
+const URL = 'http://146.190.86.208:8088/api';
+const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NDkyODU0MDMsImlhdCI6MTc0ODY4MDYwMywiZW1haWwiOiJsZXRvYW4yODVAZ21haWwuY29tIiwiaWQiOjF9.ra71uAhxbrQJQ863D-tiNZirFWaqdKxeHqO_FtLivJ0';
 
 function App() {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState<Message[]>([]);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+
+
+  const [user, setUser] = useState<any>(null);
+
+  const [rooms, setRooms] = useState<any[]>([]);
+
+  useEffect(() => {
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${TOKEN.trim()}`,
+    };
+    axios.get(`${URL}/messages/room/5`, { headers }).then((res) => {
+      console.log('MESSSGe', res.data.data);
+      setMessages(res.data.data);
+      setRooms([1, 2, 3])
+    })
+  }, [])
 
   const handleMessage = useCallback((data: WebSocketMessageResult) => {
     switch (data.type) {
@@ -54,6 +75,16 @@ function App() {
         setMessages(newMessages || []);
         break;
       }
+      case 'chatMessage': {
+        const newMsg = {
+          id: data.message_id || crypto.randomUUID(),
+          content: data.content,
+          type: 'chatMessage',
+          timestamp: Date.now(),
+        };
+        setMessages((prev) => [...prev, newMsg]);
+        break;
+      }
       default:
         break;
     }
@@ -65,8 +96,8 @@ function App() {
     onError: (error) => {
       console.error('WebSocket error:', error);
     },
-    onClose: () => {},
-    onOpen: () => {},
+    onClose: () => { },
+    onOpen: () => { },
   });
 
   const handleSendMessage = useCallback(() => {
@@ -85,7 +116,14 @@ function App() {
     setMessage('');
   }, [message, sendMessage]);
 
+
+
   const handleJoinRoom = async () => {
+
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${TOKEN.trim()}`,
+    };
     try {
       const response = await fetch(`${URL}/rooms/join/${ROOM_ID}`, {
         method: 'POST',
@@ -94,6 +132,7 @@ function App() {
           Authorization: `Bearer ${TOKEN.trim()}`,
         },
       });
+      axios.post(`${URL}/rooms/join/${ROOM_ID}`, {}, { headers: headers })
 
       console.log('Response status:', response.status);
       console.log('Response headers:', response.headers);
@@ -114,6 +153,13 @@ function App() {
     }
   };
 
+  const handleLogin = async () => {
+    axios.post('https://tefihub-api.tefibit.com/api/auth/login', { email, password }).then((res) => {
+      setUser(res.data.data);
+    }).catch((err) => { console.log(err) });
+
+  }
+
   return (
     <div className='chat-container'>
       <div
@@ -129,9 +175,30 @@ function App() {
           Status: {isConnected ? 'Connected' : 'Disconnected'}
           {error && <span className='error'> (Error occurred)</span>}
         </div>
-        <button onClick={handleJoinRoom} style={{ height: '100%' }}>
-          Join Room {ROOM_ID}
-        </button>
+
+        {
+          user ? (
+            <button onClick={handleJoinRoom} style={{ height: '100%' }}>
+              User: {user.user_id} - Join Room {ROOM_ID}
+            </button>
+          ) : (
+            <div>
+              <div>
+                <input type="text" placeholder='Email...' onChange={(e) => setEmail(e.target.value)} />
+              </div>
+              <div>
+                <input type="text" placeholder='Password...' onChange={(e) => setPassword(e.target.value)} />
+              </div>
+
+              <button onClick={handleLogin} style={{ height: '100%' }}>
+                Login
+              </button>
+
+            </div>
+
+          )
+        }
+
       </div>
 
       <div className='messages-container'>
